62cfd27cd4343e029a1ddd8962cef11fc2a11eae
Almaas Izdihar <almaasizdihar@gmail.com>
2024-06-19 02:48:56 +0700

    fix crud base class

--- app/crud/base.py
+++ app/crud/base.py
@@ -5,14 +5,38 @@
 # from sqlalchemy.orm import Session
 
 
-class CRUDBase():
-    def __init__(self):
-        """
-        CRUD object with default methods to Create, Read, Update, Delete (CRUD).
+# class CRUDBase():
+#     def __init__(self):
+#         """
+#         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
 
-        **Parameters**
+#         **Parameters**
 
-        * `model`: A SQLAlchemy model class
-        * `schema`: A Pydantic model (schema) class
-        """
-        pass
+#         * `model`: A SQLAlchemy model class
+#         * `schema`: A Pydantic model (schema) class
+#         """
+#         pass
+
+from typing import Generic, TypeVar, Type
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from app.core.db import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+    def get(self, db: Session, id: int) -> ModelType:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def create(self, db: Session, obj_in: CreateSchemaType) -> ModelType:
+        obj_in_data = obj_in.dict()
+        db_obj = self.model(**obj_in_data)  # type: ignore
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
