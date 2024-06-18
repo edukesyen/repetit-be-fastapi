ef8231c01b8b88f6117fb5b45cff4f4e8a1c0ffb
Almaas Izdihar <almaasizdihar@gmail.com>
2024-06-19 02:45:36 +0700

    fix db connection

--- app/main.py
+++ app/main.py
@@ -1,29 +1,42 @@
-# import sentry_sdk
-from fastapi import FastAPI
-from fastapi.routing import APIRoute
-from starlette.middleware.cors import CORSMiddleware
+# # import sentry_sdk
+# from fastapi import FastAPI
+# from fastapi.routing import APIRoute
+# from starlette.middleware.cors import CORSMiddleware
 
+# from app.api.v1.main import api_router
+# from app.core.config import settings
+
+# def custom_generate_unique_id(route: APIRoute) -> str:
+#     return f"{route.tags[0]}-{route.name}"
+
+# app = FastAPI(
+#     title=settings.PROJECT_NAME,
+#     openapi_url=f"{settings.API_V1_STR}/openapi.json",
+#     generate_unique_id_function=custom_generate_unique_id,
+# )
+
+# # Set all CORS enabled origins
+# if settings.BACKEND_CORS_ORIGINS:
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
+
+# app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+from fastapi import FastAPI
 from app.api.v1.main import api_router
+from app.core.db import Base, engine
 from app.core.config import settings
 
+app = FastAPI()
 
-def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
-
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    generate_unique_id_function=custom_generate_unique_id,
-)
-
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)  # Ensures all tables are created
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
